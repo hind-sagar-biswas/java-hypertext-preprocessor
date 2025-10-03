081c1c4717c9cced7041dd6a6c5dc5c80e4dcd21
Hind Sagar Biswas <hindsbhk@gmail.com>
2025-10-03 14:30:37 +0600

    Initial commit with Maven project setup

---
+++ src/main/java/com/hindbiswas/java-hypertext-preprocessor/App.java
@@ -0,0 +1,13 @@
+package com.hindbiswas.java-hypertext-preprocessor;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
